69d908af53adb7a5f5a8cd4b755e909c290316f9
Yoshihiko Kuga <horrormovies.noga@gmail.com>
2022-09-26 19:21:16 +0900

    feat: 各種番号など取得

--- scripts/prepare-commit-msg.sh
+++ scripts/prepare-commit-msg.sh
@@ -18,8 +18,12 @@ TICKET_PREFIX="ISS-"
 # ブランチ名を取得
 # https://stackoverflow.com/questions/6245570/how-to-get-the-current-branch-name-in-git#answer-12142066
 branch=$(git rev-parse --abbrev-ref HEAD)
-# `/` によりブランチ名を分割してチケット番号を取得
-ticket=$(echo $branch | cut -d / -f $TICKET_COL)
+# Issue, チケットの番号
+ticket_number=$(echo $branch | perl -p -e "s/(($GITHUB_BRANCH_PREFIX|$TICKET_PREFIX)(\d+))?.*/\$3/g")
+# GitHub 向けのブランチ中の Issue
+github_issue=$(echo $branch | perl -p -e "s/(($GITHUB_BRANCH_PREFIX)(\d+))?.*/\$1/g")
+# Jira などのチケット番号
+ticket=$(echo $branch | perl -p -e "s/(($TICKET_PREFIX)(\d+))?.*/\$1/g")
 # ブランチ自体にチケット番号が含まれているかの確認
 ticket_exists=$(echo $ticket | perl -p -e "s/^(${TICKET_PREFIX})?.*/\$1/g")
 
