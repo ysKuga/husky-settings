8c52c32acbf1e42c578ae87d6880a53db506a77f
Yoshihiko Kuga <horrormovies.noga@gmail.com>
2022-09-26 19:59:30 +0900

    #21 feat: `copy.sh` のファイルコピーまで

---
+++ scripts/copy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+echo "husky の設定"
+
+# 当ファイルのディレクトリのパス
+SCRIPTS_DIR=$(
+  cd "$(dirname "$0")"
+  pwd
+)
+SOURCE_DIR=$(dirname $SCRIPTS_DIR)
+
+# 設定対象となる実行ディレクトリ
+TARGET_DIR=$(pwd)
+
+# 移動先のディレクトリを確定する
+mkdir -p $TARGET_DIR/.husky
+mkdir -p $TARGET_DIR/scripts
+
+# .husky 配下の移動
+cp $SOURCE_DIR/.husky/pre-commit $TARGET_DIR/.husky/.
+cp $SOURCE_DIR/.husky/prepare-commit-msg $TARGET_DIR/.husky/.
+
+# scripts 配下の移動
+cp $SOURCE_DIR/scripts/pre-commit.sh $TARGET_DIR/scripts/pre-commit.sh
+cp $SOURCE_DIR/scripts/prepare-commit-msg.sh $TARGET_DIR/scripts/prepare-commit-msg.sh
