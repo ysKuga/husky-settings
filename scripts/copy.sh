81c961af9c91eb38c49bd5ffbdcb26b83f698f91
Yoshihiko Kuga <horrormovies.noga@gmail.com>
2022-09-26 20:04:39 +0900

    #21 feat: メッセージ追加

--- scripts/copy.sh
+++ scripts/copy.sh
@@ -24,3 +24,12 @@ cp $SOURCE_DIR/.husky/prepare-commit-msg $TARGET_DIR/.husky/.
 cp $SOURCE_DIR/scripts/prepare.sh $TARGET_DIR/scripts/prepare.sh
 cp $SOURCE_DIR/scripts/pre-commit.sh $TARGET_DIR/scripts/pre-commit.sh
 cp $SOURCE_DIR/scripts/prepare-commit-msg.sh $TARGET_DIR/scripts/prepare-commit-msg.sh
+
+echo "以下を実行してください。"
+echo ""
+echo "- npx husky install"
+echo ""
+echo "以下の記述を package.json の scripts に追加してください。"
+echo ""
+echo "    \"prepare\": \"sh ./scripts/prepare.sh\","
+echo ""
