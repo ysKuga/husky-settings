76aea4103e7ae0d13a5130e97275bdb1a6af63e1
Yoshihiko Kuga <horrormovies.noga@gmail.com>
2024-10-22 22:24:02 +0900

    #34 chore: コピースクリプトの修正

--- scripts/copy-yarn-workspaces.sh
+++ scripts/copy-yarn-workspaces.sh
@@ -13,7 +13,6 @@ SOURCE_DIR=$(dirname $SCRIPTS_DIR)
 TARGET_DIR=$(pwd)
 
 # 移動先のディレクトリを確定する
-mkdir -p $TARGET_DIR/.husky
 mkdir -p $TARGET_DIR/scripts
 # workspaces の格納対象
 mkdir -p $TARGET_DIR/libs
@@ -21,25 +20,12 @@ mkdir -p $TARGET_DIR/libs
 # lefthook.yml の移動
 cp $SOURCE_DIR/lefthook.yml $TARGET_DIR/lefthook.yml
 
-# .husky 配下の移動
-cp $SOURCE_DIR/.husky/pre-commit $TARGET_DIR/.husky/.
-cp $SOURCE_DIR/.husky/prepare-commit-msg $TARGET_DIR/.husky/.
-
 # scripts 配下の移動
 cp $SOURCE_DIR/scripts/prepare.sh $TARGET_DIR/scripts/prepare.sh
 cp -r $SOURCE_DIR/scripts/husky/ $TARGET_DIR/scripts/.
 cp -r $SOURCE_DIR/package.json $TARGET_DIR/libs/husky-settings/.
 cp -r $SOURCE_DIR/yarn.lock $TARGET_DIR/libs/husky-settings/.
 
-echo "以下を実行してください。"
-echo ""
-echo "- npx husky install"
-echo "- npx lefthook install"
-echo ""
-echo "以下の記述を package.json の scripts に追加してください。"
-echo ""
-echo "    \"prepare\": \"sh ./scripts/prepare.sh\","
-echo ""
 echo "以下の記述を package.json の workspaces に追加してください。"
 echo ""
 echo "    \"workspaces\": [\"libs/husky-settings\"],"
--- scripts/copy.sh
+++ scripts/copy.sh
@@ -13,26 +13,13 @@ SOURCE_DIR=$(dirname $SCRIPTS_DIR)
 TARGET_DIR=$(pwd)
 
 # 移動先のディレクトリを確定する
-mkdir -p $TARGET_DIR/.husky
 mkdir -p $TARGET_DIR/scripts
 
 # lefthook.yml の移動
 cp $SOURCE_DIR/lefthook.yml $TARGET_DIR/lefthook.yml
 
-# .husky 配下の移動
-cp $SOURCE_DIR/.husky/pre-commit $TARGET_DIR/.husky/.
-cp $SOURCE_DIR/.husky/prepare-commit-msg $TARGET_DIR/.husky/.
 
 # scripts 配下の移動
 cp $SOURCE_DIR/scripts/prepare.sh $TARGET_DIR/scripts/prepare.sh
 cp -r $SOURCE_DIR/scripts/husky/ $TARGET_DIR/scripts/.
 
-echo "以下を実行してください。"
-echo ""
-echo "- npx husky install"
-echo "- npx lefthook install"
-echo ""
-echo "以下の記述を package.json の scripts に追加してください。"
-echo ""
-echo "    \"prepare\": \"sh ./scripts/prepare.sh\","
-echo ""
